93dd5330227450fca1f2740611d47695ffbf0363
Leesoo <altnfsudwo@naver.com>
2021-11-24 10:45:16 +0900

    Create week10_env_test.c
    환경변수

---
+++ week10_env_test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+extern char** environ; //환경변수로 사용되는 전역변수
+
+void main(int argc, char* argv[], char* envp[]){
+    int i = 0;
+    char* v[2];
+    char* newenv[3];
+
+    if(argc < 2) return;
+    v[0] = "/usr/bin/env";
+    v[1] = NULL;
+    newenv[0] = "AAA=aaa";
+    newenv[1] = "BBB=bbb";
+    newenv[1] = NULL;
+    switch(argv[1][0]){
+        case '2':
+            execve(v[0], v, newenv);
+            break;
+        case '3':
+            execve(v[0], v, environ);
+            break;
+        default:
+            execve(v[0], v, NULL);
+            break;
+    }
+}
